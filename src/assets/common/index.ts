5ecb02fbabfe353d855c816b570aa04032d16c52
mac <mac@macdeMacBook-Pro.local>
2022-06-28 20:01:23 +0800

    首页

--- src/assets/common/index.ts
+++ src/assets/common/index.ts
@@ -1,9 +1,13 @@
-import ETH from './ETH.png';
 import banner_explore from './banner_explore.png';
 import banner_preview from './banner_preview.png';
 import bg_btn_mint from './bg_btn_mint.png';
 import discord from './discord.png';
+import ETH from './ETH.png';
 import icon_down from './icon_down.png';
+import home_icon from './icon_home_bg.png';
+import home_text from './icon_home_text.png';
+import home_text1 from './icon_home_text1.png';
+import homeTop from './icon_home_top.png';
 import telegram from './telegram.png';
 import twitter from './twitter.png';
 import user_icon from './user_icon.png';
@@ -20,5 +24,9 @@ const ImageCommon: any = {
   twitter,
   user_icon,
   website,
+  home_icon,
+  home_text,
+  home_text1,
+  homeTop,
 };
 export default ImageCommon;
